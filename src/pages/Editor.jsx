7934d752a4d499ba2835fc8bf61eecabc82582bd
Damon <damonlhill82@gmail.com>
2022-08-15 10:58:38 +1000

    implemented rich text editor with dummy text

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { HtmlEditor, Image, Inject, Link, QuickToolbar,
+  RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
+
+import { EditorData } from '../data/dummy';
+import { Header } from '../components';
 
 const Editor = () => {
   return (
-    <div>Editor</div>
-  )
-}
+    <div className="rounded-3xl bg-white m-2 p-2 mt-24 md:m-10 md:p-10">
+      <Header
+        category="App"
+        title="Editor"
+      />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={ [HtmlEditor, Toolbar, Image, Link, QuickToolbar] } />
+      </RichTextEditorComponent>
+    </div>
+  );
+};
 
-export default Editor
+export default Editor;
